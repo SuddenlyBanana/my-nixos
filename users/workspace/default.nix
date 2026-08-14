9340feae4b6bc252569dee21ca0ede83f043c4c3
SuddenlyBanana <SuddenlyBanana@proton.me>
2026-08-14 17:17:24 +0200

    change user access

--- users/workspace/default.nix
+++ users/workspace/default.nix
@@ -8,7 +8,6 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIijmUFfAZbhbcFMnWSFyM0NEUviWiVEvCO1qB/jra+/ SuddenlyBanana@proton.me"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKPQ80PQ7ZhPmQm9PJ4DLebxsVX8WvChA61twLuzYbH3 teto"
     ];
   };
 }
