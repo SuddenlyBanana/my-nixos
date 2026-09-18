f62bd83fec54f9a7ec74c09cc299a550279c7e1a
SuddenlyBanana <SuddenlyBanana@proton.me>
2026-09-18 02:00:13 +0200

    add workspace to nix trusted users

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -6,6 +6,8 @@
   networking.useNetworkd = true;
   security.sudo.wheelNeedsPassword = false;
 
+  nix.settings.trusted-users = [ "root" "workspace" ];
+
   # Servers use resolved as their sole mDNS responder.  Do not enable Avahi
   # here as running both responders causes .local hostname conflicts.
   services.resolved = {
